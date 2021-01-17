4654f7641fbc6b276615b53fe3f3ccc4adf21375
jlsajfj <jlsajfj@hotmail.com>
2021-01-17 11:15:46 -0500

    formatted output and created output to file. converted received data to dict

--- orzNetwork.py
+++ orzNetwork.py
@@ -1,12 +1,14 @@
 from pwn import *
+import json
 
-
-r = remote('chall.ctf.bamboofox.tw', 10369, level = 'debug')
+r = remote('chall.ctf.bamboofox.tw', 10369)#, level = 'debug')
 
 r.recvline()
 line = r.recvline()
 
 prefix = line[7:23].decode()
+
+print('starting proof of work')
 
 import hashlib
 difficulty = 20
@@ -28,11 +30,58 @@ while True:
         break
 r.sendline(str(i))
 r.sendline()
-r.recvline()
 
+print('proof of work complete')
+
+cnt = int(r.recvline().decode().split()[6])
+
+def comp_number(line: str) -> (int,int):
+    return list(map(lambda x: int(x.split()[0]), line.split(" #")[1:]))
+
+def shared_secret(line: str):
+    s = list(map(lambda x: int(x.split()[-1]), line.split(", ")))
+    # print(s)
+    
+    return s
+
+def edge_to_dict():
+    link = comp_number(recvstr())
+    dhke = shared_secret(recvstr())
+    d = {
+        'acomp': link[0],
+        'bcomp': link[1],
+        'mod': dhke[0],
+        'base': dhke[1],
+        'apub': dhke[2],
+        'bpub': dhke[3]
+    }
+    return d
+
+def recvstr() -> str:
+    return r.recvline().decode().strip()
+
+print("{} sets\nreceiving data".format(cnt))
+son = []
 #this is actual data
-print(r.recvline().decode().split(':')[1].strip())
-print(r.recvline().decode().strip())
+temp1 = comp_number(recvstr().split(':')[1])
+temp2 = shared_secret(recvstr())
+son.append({
+    'acomp': temp1[0],
+    'bcomp': temp1[1],
+    'mod': temp2[0],
+    'base': temp2[1],
+    'apub': temp2[2],
+    'bpub': temp2[3]
+})
+
+for _ in range(cnt-1):
+    son.append(edge_to_dict())
+
+print('sets received\nwriting to file')
+
+f = open('orzNetwork.out','w')
+f.write('\n'.join(map(json.dumps, son)))
+f.close()
 
 """
 Secure connection established between computer #195 (Alice) and computer #246 (Bob).
